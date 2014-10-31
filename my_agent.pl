2666e19e5e540a9f8aecdcb874d6c5bc43b49219
me <li3939108@gmail.com>
2014-10-31 12:52:28 -0500

    more score

--- my_agent.pl
+++ my_agent.pl
@@ -1,5 +1,7 @@
 % This is a Prolog file
-% my_agent.pl
+
+%
+% Chaofan Li 2014 <chaof@tamu.edu>
 %
 
 %   this procedure requires the external definition of two procedures:
@@ -12,9 +14,13 @@
 %                 executed.
 %
 % This is what should be fleshed out
+%
 
+
+% load the utils.pl
+% use the random
 :- include('utils.pl').
-debug:- false.
+
 % parameter for how many times of random turn to try
 turn_times_max(10).
 
@@ -22,6 +28,10 @@ turn_times_max(10).
 % if returning to origin morn than this parameter
 % climb out
 origin_max(10).
+
+% if true , enter debug mode
+% show the world
+debug:- false.
 	 
 safe(X,Y):-
 	( safe_fact(X,Y);
@@ -46,12 +56,14 @@ init_agent:-
 	retractall(gold(_)),
 	retractall(original_loc_times(_)),
 	retractall(arrow(_)),
+	retractall(wumpus_number(_)),
 	assert(current_location(1,1)),
 	assert(my_angle(0)),
 	assert(visited(1,1)) ,
 	assert(random_turn_times(0)),
 	assert(original_loc_times(0)),
 	assert(arrow(1)),
+	assert(wumpus_number(1)),
 	assert(gold(0) ) 
 .
 
@@ -87,11 +99,35 @@ forward_location:-
 	( Angle = 180, X1 is X - 1, assert( current_location(X1, Y) ) );
 	( Angle = 270, Y1 is Y - 1, assert( current_location(X, Y1) ) )  )
 .
+run_agent([_,no|_], _):-
+	current_location(X,Y), assert( safe_fact(X, Y) ), 
+	Xplus is X + 1, Yplus is Y + 1, Xminus is X - 1, Yminus is Y - 1,
+	assert(no_pit(Xplus, Y) ), assert( no_pit(X, Yplus) ), assert( no_pit(Xminus, Y)) , assert( no_pit(X, Yminus) ),
+	fail
+.
+run_agent([Stench|_], _):-current_location(X,Y),  
+	Xplus is X + 1, Yplus is Y + 1, Xminus is X - 1, Yminus is Y - 1,
+	(Stench = no;( Stench = yes, wumpus_number(N), N = 0)),
+	assert(no_wumpus(Xplus, Y) ), assert( no_wumpus(X, Yplus) ), 
+	assert( no_wumpus(Xminus, Y)) , assert( no_wumpus(X, Yminus) ),
+	assert( safe_fact(X, Y) ),
+	fail
+.
 run_agent(_,_):- current_location(X, Y), X = 1, Y = 1,
 	original_loc_times(Original_loc_T), 
 	NewT is Original_loc_T + 1, 
 	retract(original_loc_times(Original_loc_T)),
 	assert(original_loc_times(NewT) ),
+	fail
+.
+run_agent([_,_,_,_,yes], _):-
+	wumpus_number(N), NewN is N - 1,
+	retract(wumpus_number(N)),
+	assert(wumpus_number(NewN)),
+	fail
+.
+run_agent([_,_,_,yes,_], _):-
+	retractall(current_location(_, _) ), previous_location(X, Y), assert(current_location(X, Y) ), 
 	fail
 .
 run_agent(_,climb ):- current_location(X, Y), my_angle(Angle),X = 1, Y = 1,
@@ -104,6 +140,7 @@ run_agent([_,_,yes,_,_], grab ):- current_location(X, Y),my_angle(Angle),
 	assert(safe_fact(X, Y) ), gold(N), NewNumberOfGold is N + 1, assert(gold(NewNumberOfGold)),
 	( ( debug, display_world,  write(current_location(X,Y)), write(my_angle(Angle)) ) ; true)
 .
+	
 run_agent([yes|_], shoot):-current_location(X, Y), my_angle(Angle), 
 	X = 1, Y = 1, arrow(N) , N>0, (Angle = 0; Angle = 90) , NewN is N - 1, retractall(arrow(N)), assert(arrow(NewN)),
 	( ( debug, display_world, write(current_location(X,Y)), write(my_angle(Angle)) ) ; true)
@@ -111,19 +148,7 @@ run_agent([yes|_], shoot):-current_location(X, Y), my_angle(Angle),
 %		(is_wumpus(_, _),is_wumpus(Xw, Yw),
 %		(	(X = Xw, ( (Y < Yw, Angle = 90 );( Y > Yw, Angle = 270)) ) ;
 %			(Y = Yw, ( (X < Xw, Angle = 0 );( X > Xw, Angle = 180)) ) )  ) )
-run_agent([_,no|_], _):-
-	current_location(X,Y), assert( safe_fact(X, Y) ), 
-	Xplus is X + 1, Yplus is Y + 1, Xminus is X - 1, Yminus is Y - 1,
-	assert(no_pit(Xplus, Y) ), assert( no_pit(X, Yplus) ), assert( no_pit(Xminus, Y)) , assert( no_pit(X, Yminus) ),
-	fail.
-run_agent([no|_], _):-
-	current_location(X,Y), assert( safe_fact(X, Y) ), 
-	Xplus is X + 1, Yplus is Y + 1, Xminus is X - 1, Yminus is Y - 1,
-	assert(no_wumpus(Xplus, Y) ), assert( no_wumpus(X, Yplus) ), assert( no_wumpus(Xminus, Y)) , assert( no_wumpus(X, Yminus) ),
-	fail.
-run_agent([_,_,_,yes,_], _):-
-	retractall(current_location(_, _) ), previous_location(X, Y), assert(current_location(X, Y) ), 
-	fail.
+
 run_agent([_,_,_,yes,_], Action):- my_angle(Angle),current_location(X, Y), (
 	( Action = turnleft,
 	( (Angle = 0, Y = 1 );
